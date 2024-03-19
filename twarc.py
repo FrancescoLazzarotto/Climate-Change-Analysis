a7fcc2ecd4e9a234da776158f5f0598f05b37897
Francesco Lazzarotto <82331651+FrancescoLazzarotto@users.noreply.github.com>
2024-03-19 08:29:34 +0100

    Update and rename model.py to twarc.py

--- twarc.py
+++ twarc.py
@@ -3,12 +3,12 @@ import pdb
 from twarc.client2 import Twarc2
 from twarc.expansions import ensure_flattened
 
-# Funzione per recuperare il testo del tweet utilizzando Twarc
-def get_tweet_text(tweet_id):
-    # Inizializza Twarc con il tuo bearer token
-    t = Twarc2(bearer_token="AAAAAAAAAAAAAAAAAAAAAInksQEAAAAASNA7s%2FXHNuvVKxXf7Fxl102Lv3E%3DmWuZSgrGxBKt1WcTnzV9zBiPpefl9cuevV6gqIwdZvZihG0TKN")
 
-    # Utilizza Twarc per ottenere il testo del tweet
+def get_tweet_text(tweet_id):
+    #put your brearer token from your twitter developer account
+    t = Twarc2(bearer_token="")
+
+    #using twarc to get the text 
     try:
         tweet = t.tweet(tweet_id)
         if "data" in tweet:
@@ -19,17 +19,17 @@ def get_tweet_text(tweet_id):
         print("Errore durante il recupero del tweet:", e)
         return None
 
-# Percorso completo del file CSV
+#path of the dataset
 csv_file_path = "C:/Users/checc/Desktop/api/The Climate Change Twitter Dataset.csv"
 
-# Leggi il file CSV e recupera il testo dei tweet per gli ID forniti
+#read the csv file and extract the text from the ids
 try:
     with open(csv_file_path, 'r', encoding='utf-8') as file:
         csv_reader = csv.reader(file)
-        next(csv_reader)  # Salta l'intestazione se presente
-        for _ in range(10):  # Recupera solo le prime 10 righe
+        next(csv_reader) 
+        for _ in range(10): 
             row = next(csv_reader)
-            tweet_id = row[1]  # Supponiamo che l'ID del tweet sia nella seconda colonna
+            tweet_id = row[1] #put the column where the ids of the tweets are
             tweet_text = get_tweet_text(tweet_id)
             if tweet_text:
                 print("Testo del tweet:", tweet_text)
